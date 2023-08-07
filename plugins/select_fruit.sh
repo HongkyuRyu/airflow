a3ac9981fcb220b980250d1609b72c65e6f9436c
HongkyuRyu <minsubrother@naver.com>
2023-08-07 21:09:30 +0900

    feats: plugins폴더 추가

---
+++ plugins/select_fruit.sh
@@ -0,0 +1,12 @@
+
+FRUIT=$1
+if [ "$FRUIT" == "APPLE" ]; then
+        echo "YOU SELECTED APPLE"
+elif [ "$FRUIT" == "ORANGE" ]; then
+        echo "YOU SELECTED ORANGE"
+elif [ "$FRUIT" == "GRAPE" ]; then
+        echo "YOU SELECTED GRAPE"
+else
+        echo "YOU SELECTED OTHER FRUITS"
+fi
+
