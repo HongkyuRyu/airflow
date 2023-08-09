24564843e9e1fe06433561caaff644ecb547b556
HongkyuRyu <minsubrother@naver.com>
2023-08-09 21:50:44 +0900

    feat: op_args

---
+++ dags/dags_python_with_op_args.py
@@ -0,0 +1,20 @@
+# 일반 파라미터 + args
+
+from airflow import DAG
+import pendulum
+from airflow.operators.python import PythonOperator
+from common.common_func import regist
+with DAG(
+    dag_id="dags_python_with_op_args",
+    schedule="30 6 * * *",
+    start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
+    catchup=False
+) as dag:
+    
+    regist_t1 = PythonOperator(
+        task_id = "regist_t1",
+        python_callable=regist,
+        op_args=["hongkyu", "man", "kr", "seoul"]
+    )
+    
+    regist_t1
