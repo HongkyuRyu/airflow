fd963f5ef2a2e57a8cc9f7475b49eed5ed8284c1
HongkyuRyu <minsubrother@naver.com>
2023-08-09 20:51:52 +0900

    chore: 코드 수정

--- dags/dags_python_operator.py
+++ dags/dags_python_operator.py
@@ -7,7 +7,7 @@ import random
 with DAG(
     dag_id="dags_python_operator",
     schedule="30 6 * * *", # daily 6:30
-    start_date=pendulum.datetime(2023, 3, 1, tz="Asiz/Seoul"),
+    start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
     catchup=False
 ) as dag:
     # 파이썬 함수 정의
